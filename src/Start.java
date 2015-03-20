781ac71fdec54cbef4246a621caf1a87762f991b
mmatuk <coolskater6088@hotmail.com>
2015-03-19 21:39:54 -0400

    Added the Start.java class file to the project. The file does not comtain anything other than a simple print line.

---
+++ src/Start.java
@@ -0,0 +1,20 @@
+//***********************************************************
+// 	Start.java								Team 2 
+//	Matt Matuk, Wyatt leith, Nick Tsang
+//
+//	File Created by: Matt Matuk				March 19, 2015
+//
+//	This is the casino game that we are creating.
+//	This class will be the main driver of the program that
+//	will start the game.
+//***********************************************************
+
+
+public class Start 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Testing.....");
+	}
+
+}
